887398209bfdf82642f583caaa8cc53ee5fa4860
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-12-10 22:45:28 -0600

    Activity: Rest-Rant Administrator Account

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -107,19 +107,18 @@ function PlaceDetails() {
 
 	let placeActions = null
 
-	if (currentUser?.role === 'admin') {
-		placeActions = (
-			<>
-				<a className="btn btn-warning" onClick={editPlace}>
-					Edit
-				</a>{` `}
-				<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-					Delete
-				</button>
-			</>
-		)
-	}
-
+if (currentUser?.role === 'admin') {
+    placeActions = (
+        <>
+            <a className="btn btn-warning" onClick={editPlace}>
+                Edit
+            </a>
+            <button type="submit" className="btn btn-danger" onClick={deletePlace}>
+                Delete
+            </button>
+        </>
+    )
+}
 
 	return (
 		<main>
